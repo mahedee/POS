fd7f4f068a206fb844e46a201635bf0182745dd4
Priom Biswas <priombiswas@yahoo.com>
2016-04-11 12:07:00 +0600

    New project created
    New project is created with Login, Registration, Brand, Category,
    Measurement, Product, FinancialYear and Shop models, controllers and
    views

---
+++ POS/POS/Migrations/201604101439553_firstMigration.cs
@@ -0,0 +1,16 @@
+namespace POS.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class firstMigration : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ POS/POS/Models/Brand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace POS.Models
+{
+    [Table("Brand")]
+    public class Brand : CommonProperties
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Brand Name is Required")]
+        [Display(Name = "Brand Name")]
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Create/Modification Date is required.")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",
+                       ApplyFormatInEditMode = true)]
+        [Display(Name = "Create/Modification Date")]
+        public DateTime ModifiedDate { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
