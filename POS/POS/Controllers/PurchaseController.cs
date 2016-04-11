b3c388b13f23d7370cafc8de5ee2bca2b48d426f
Priom Biswas <priombiswas@yahoo.com>
2016-04-11 14:17:36 +0600

    Purchase controller and index view
    Purchase controller and index view added

---
+++ POS/POS/Controllers/PurchaseController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using POS.Models;
+
+namespace POS.Controllers
+{
+    public class PurchaseController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Purchase
+        public ActionResult Index()
+        {
+            var supplierList = db.Suppliers.ToList();
+            ViewBag.Suppliers = supplierList;
+
+            var productList = db.Products.ToList();
+            ViewBag.Products = productList;
+            return View();
+        }
+    }
+}
