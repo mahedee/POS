e68875969a8c80b1144ffb88324add1c4add4fee
Priom Biswas <priombiswas@yahoo.com>
2016-04-11 17:00:08 +0600

    Jquery Save function
    The function is not working now. Should be checked by team members

--- POS/POS/Controllers/PurchaseController.cs
+++ POS/POS/Controllers/PurchaseController.cs
@@ -20,5 +20,39 @@ namespace POS.Controllers
             ViewBag.Products = productList;
             return View();
         }
+
+        [HttpPost]
+        public JsonResult SavePurchase(Purchase objPurchase)
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                using (db)
+                {
+                    db.Purchases.Add(objPurchase);
+                    db.SaveChanges();
+                    status = true;  
+                }
+                
+                //using (DemoDBContext dc = new DemoDBContext())
+                //{
+                //    //////Order order = new Order { OrderNo = O.OrderNo, OrderDate = O.OrderDate, Description = O.Description };
+                //    //////foreach (var i in objOrder.OrderDetails)
+                //    //////{
+                //    //////    //
+                //    //////    // i.TotalAmount = 
+                //    //////    order.OrderDetails.Add(i);
+                //    //////}
+                //    dc.Order.Add(objOrder);
+                //    dc.SaveChanges();
+                //    status = true;
+                //}
+            }
+            else
+            {
+                status = false;
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
     }
 }
