58039dfe76d3687b9f2313d556199bcc5b07b1a9
ronycwy98 <rony.cwy98@gmail.com>
2018-04-20 21:03:43 +0600

    initial commit

---
+++ Batch11/POS/POS/Controllers/CompanyInfoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using POS.Models;
+
+namespace POS.Controllers
+{
+    public class CompanyInfoesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: CompanyInfoes
+        public ActionResult Index()
+        {
+            return View(db.CompanyInfos.ToList());
+        }
+
+        // GET: CompanyInfoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CompanyInfo companyInfo = db.CompanyInfos.Find(id);
+            if (companyInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(companyInfo);
+        }
+
+        // GET: CompanyInfoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CompanyInfoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CompanyCode,CompanyName,Address,ShortName")] CompanyInfo companyInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CompanyInfos.Add(companyInfo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(companyInfo);
+        }
+
+        // GET: CompanyInfoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CompanyInfo companyInfo = db.CompanyInfos.Find(id);
+            if (companyInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(companyInfo);
+        }
+
+        // POST: CompanyInfoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CompanyCode,CompanyName,Address,ShortName")] CompanyInfo companyInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(companyInfo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(companyInfo);
+        }
+
+        // GET: CompanyInfoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CompanyInfo companyInfo = db.CompanyInfos.Find(id);
+            if (companyInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(companyInfo);
+        }
+
+        // POST: CompanyInfoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CompanyInfo companyInfo = db.CompanyInfos.Find(id);
+            db.CompanyInfos.Remove(companyInfo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Batch11/POS/POS/Models/CompanyInfoViewModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace POS.Models
+{
+    public class CompanyInfo
+    {
+
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CompanyCode { get; set; }
+
+        public string CompanyName { get; set; }
+        public string Address { get; set; }
+        public string ShortName { get; set; }
+    }
+}
